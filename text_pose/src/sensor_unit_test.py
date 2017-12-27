de72033f754518dd7c9588c8f971984d5541df84
peterx7803 <peterx7803@gmail.com>
2017-12-28 03:00:15 +0800

    add caffe prediction

--- text_pose/src/sensor_unit_test.py
+++ text_pose/src/sensor_unit_test.py
@@ -16,8 +16,21 @@ from cv_bridge import CvBridge, CvBridgeError
 import message_filters
 import math
 
-rospy.init_node('sensor_unit_test', anonymous=True)
 
+# --- caffe dep ---#
+import sys
+sys.path.append('/home/peter/caffe/python') # pycaffe 
+sys.path.append('/home/peter/caffe/fcn.berkeleyvision.org') # bvlc fcn module path
+from PIL import Image
+import caffe
+import os
+# --- caffe dep ---#
+
+
+
+
+
+rospy.init_node('sensor_unit_test', anonymous=True)
 
 
 # Publisher for publishing images in rviz
@@ -31,6 +44,13 @@ msg = rospy.wait_for_message('/camera/depth/camera_info', CameraInfo, timeout=No
 #     [fx'  0  cx' Tx]
 # P = [ 0  fy' cy' Ty]
 #     [ 0   0   1   0]
+
+
+
+
+# load net
+net = caffe.Net('voc-fcn8s/deploy.prototxt', 'voc-fcn8s/fcn8s-heavy-pascal.caffemodel', caffe.TEST)
+
 
 def main():
     image_sub = message_filters.Subscriber('image', Image)
@@ -46,6 +66,24 @@ def brand_prediction(rgb_data, depth_data):
 
     try:
         cv_image = cv_bridge.imgmsg_to_cv2(rgb_data, "bgr8")
+	
+	# load image, switch to BGR, subtract mean, and make dims C x H x W for Caffe
+	im = cv_image
+	in_ = np.array(im, dtype=np.float32)
+	in_ = in_[:,:,::-1]
+	in_ -= np.array((104.00698793,116.66876762,122.67891434))
+	in_ = in_.transpose((2,0,1))
+	# shape for input (data blob is N x C x H x W), set data
+	net.blobs['data'].reshape(1, *in_.shape)
+	net.blobs['data'].data[...] = in_
+	# run net and take argmax for prediction
+	net.forward()
+	out = net.blobs['score'].data[0].argmax(axis=0)
+
+	image = Image.fromarray(net.blobs[layer].data[0].argmax(0).astype(np.uint8), mode='P')
+	mask = mask_convertion(image)
+        cv_mask = cv2_to_imgmsg(mask, encoding="passthrough")   #grays scale
+
     except CvBridgeError as e:
         print(e)
     '''
@@ -55,7 +93,24 @@ def brand_prediction(rgb_data, depth_data):
     '''
 
     point_publisher.publish(depth_data)
-    mask_publisher.publish(rgb_data)
+    mask_publisher.publish(cv_mask)
+
+
+def mask_convertion(unconverted_result):
+    color, count = unconverted_result.colors()
+    decreasing_count = count[np.argsort(-count)]
+    decreasing_color = color[np.argsort(-count)]
+
+    # index 0 must be background, use index 1
+    mask = unconverted_result
+    for height in range(0,481):
+        for width in range(0,641):
+		cls = rgb.getpixel((width,height))
+		if cls == decreasing_color[1]:
+			mask.putpixel((width,height),255)
+	`	else:
+			mask.putpixel((width,height),0)
+   return mask
     
     
 
